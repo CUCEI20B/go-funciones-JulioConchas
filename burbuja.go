2b22d7b76a1979d06340fd41b800a61b6dbdc0e9
jconchas <jconchas@ibm.com>
2020-10-15 13:16:25 -0500

    jconchas prueba 01

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,18 @@
 package main
 
 func Burbuja(s []int64)  {
-
+	var aux int64
+    for i := 0;i < len(s) ;i++ {
+        for j := 0;j < (len(s) - i - 1);j++ {
+            if s[j] > s[j+1] {
+                aux = s[j]
+                s[j] = s[j+1]
+                s[j+1] = aux
+            }
+        }
+    }
 }
 
 func main()  {
-	
+
 }
